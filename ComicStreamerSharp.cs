ed1e50406abb240647f6513fbc6c86a6c5c34f63
ForceConstant <bhw@forceconstant.com>
2017-02-24 19:34:12 -0500

    Added files

---
+++ ComicStreamerSharp.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace ComicStreamer
+{
+    public class ComicStreamerSharp
+    {
+        public ComicStreamerSharp(string asBaseUrl)
+        {
+            msBaseUrl = asBaseUrl;
+        }
+
+        private IRestResponse ExecuteCommand(string Resource)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(msBaseUrl);
+
+            var request = new RestRequest();
+            request.Resource = Resource;
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.ErrorException != null)
+            {
+                Console.WriteLine("Error retrieving response for " + Resource);
+            }
+
+            return response;
+        }
+
+        public string GetDBInfo()
+        {
+            IRestResponse response = ExecuteCommand("dbinfo");
+
+            return response.Content;
+        }
+
+        public string GetVersion()
+        {
+            IRestResponse response = ExecuteCommand("version");
+
+            return response.Content;
+        }
+
+        public string GetDeletedComics(string date)
+        {
+            /*
+                  * /deleted
+             - list of comic IDs that have been removed from the DB
+                 args:
+                         since
+                                 - date of the earliest returned value */
+
+            IRestResponse response = ExecuteCommand("deleted?since=" + date);
+
+            return response.Content;
+        }
+
+
+        public JObject GetComicInfo(string id)
+        {
+            /*     /comic/{id}
+                     - info about specific comic
+                     */
+
+            IRestResponse response = ExecuteCommand("/comic/" + id);
+
+            JObject o = JObject.Parse(response.Content);
+
+            return o;
+        }   
+
+        public byte[] GetComicPage(string id = "0", int pagenum = 0)
+        {
+            //TODO: Add height arg.
+            /*/comic/{id}/page/{pagenum}
+                     - return specific page image of specific comic
+                         args:
+                             max_height
+                                 - will resize image*/
+            IRestResponse response = ExecuteCommand("/comic/" + id + "/page/" + pagenum);
+
+            byte[] imageBytes = response.RawBytes;
+ 
+            return imageBytes;
+        }
+                 
+        public void SetBookmark(string id, string pagenum = "clear")
+        {
+            /*/comic/{id}/page/{pagenum}/bookmark
+                     - sets the time of last access and last page read for the comic.
+                         client would fetch this for each page turn
+                         if {pagenum} is "clear"  clears bookmark for the given book*/
+            IRestResponse response = ExecuteCommand("/comic/" + id + "/page/" + pagenum + "/bookmark");
+
+        }
+
+        public byte[] GetThumbnail(string id)
+        {
+            /* /comic/{id}/thumbnail
+                     - return specific small cover image of specific comic*/
+            IRestResponse response = ExecuteCommand("/comic/" + id  + "/thumbnail");
+
+            byte[] imageBytes = response.RawBytes;
+
+            return imageBytes;
+        }
+
+        public byte[] GetComicFile(string id)
+        {
+            /* /comic/{id}/file
+                     - return entire specific comic file*/
+            IRestResponse response = ExecuteCommand("/comic/" + id + "/file");
+
+            byte[] imageBytes = response.RawBytes;
+
+            return imageBytes;
+        }
+
+        /*/comiclist
+           - return list of comics info.  with no args, returns entire list
+                   args:
+                       series
+                           filter by series (wildcard accepted)
+                       title
+                           filter by issue title (wildcard accepted)
+                       path
+                           filter by file path (wildcard accepted)
+                       character
+                           filter by character (wildcard accepted)
+                       team
+                           filter by team (wildcard accepted)
+                       location
+                           filter by location (wildcard accepted)
+                       storyarc
+                           filter by story arc (wildcard accepted)
+                       genre
+                           filter by genre (wildcard accepted)
+                       tag
+                           filter by generic tag (wildcard accepted)
+                       volume
+                           filter by volume (wildcard accepted)
+                       publisher
+                           filter by publisher (wildcard accepted)
+                       credit
+                           filter by creator credit (wildcard accepted)
+                           should be a name optionally followed by a ":" and the role. e.g:
+                               credit=Brian*Vaughn
+                               credit=*Buscema:inker
+                       start_date
+                           the beginning publication date of the resultset
+                       end_date
+                           the end publication date of the resultset
+                       added_since
+                           only comics added the to database since given date
+                       lastread_since
+                           only comics that have been read since given date
+                           (Set this value to something like "1970" to filter out unread comics)
+                       per_page
+                           max amount of results to be returned
+                       offset
+                           the starting offset of the query resultset
+                       order
+                           order by given key.  if key has "-" prepended, order descending
+                           available sort keys:
+                               series, title, volume, issue, publisher, path, modified, added, lastread, date
+
+                   wildcard character is * (asterisk)
+
+                   date format is "YYYY-MM-DD hh:mm:ss", where the right-most (most granular) portions may be omitted
+/*/
+        public JObject GetComicList(
+            string series = "", 
+            string title = "",
+            string path = "",
+            string character = "",
+            string team = "",
+            string location = "",
+            string storyarc = "",
+            string genre = "",
+            string tag = "",
+            string volume = "",
+            string publisher = "",
+            string credit = "",
+            string start_date = "",
+            string end_date = "",
+            string added_since = "",
+            string lastread_Since = "",
+            string per_page = "",
+            string offset = "",
+            string order = "")
+        {
+          
+            IRestResponse response = ExecuteCommand("/comiclist?series=" + series + 
+                "&title=" + title + 
+                "&path=" + path + 
+                "&character=" + character + 
+                "&team=" + team + 
+                "&location=" + location +
+                "&storyarc=" + storyarc + 
+                "&genre=" + genre +
+                "&tag=" + tag +
+                "&volume=" + volume +
+                "&publisher=" + publisher +
+                "&credit=" + credit +
+                "&start_date=" + start_date +
+                "&end_date=" + end_date +
+                "&added_since=" + added_since +
+                "&lastread_Since=" + lastread_Since +
+                "&per_page=" + per_page +
+                "&offset=" + offset +
+                "&order=" + order);
+
+            JObject o = JObject.Parse(response.Content);
+
+            return o;
+        }
+                 
+        public JObject GetFolders(string path = "")
+        {
+            /*/folders/[path]
+                     - Return list of folders  with names and access URLS), and list of comics in the specific folder
+                       Without a path, returns just the top level folders
+                       */
+            IRestResponse response = ExecuteCommand("/folders/" + path);
+
+            JObject o = JObject.Parse(response.Content);
+
+            return o;
+        }
+
+
+        readonly string msBaseUrl;
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ComicStreamer.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        readonly string BaseUrl = "http://192.168.1.82:32500";
+
+        [TestMethod()]
+        public void GetDBInfoTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            var response = cs.GetDBInfo();
+
+            if (response.Length > 0)
+            {
+                Console.WriteLine(response);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetVersionTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            var response = cs.GetVersion();
+
+            if (response.Length > 0)
+            {
+                Console.WriteLine(response);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetDeletedComicsTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            var response = cs.GetDeletedComics("2017-2-19");
+
+            if (response.Length > 0)
+            {
+                Console.WriteLine(response);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+            response = cs.GetDeletedComics("2017-2-21");
+
+            if (response.Length > 0)
+            {
+                Console.WriteLine(response);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetComicInfoTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            var response = cs.GetComicInfo("98");
+
+            string series = (string)response["comics"][0]["series"];
+
+            Console.WriteLine(response.ToString(Formatting.Indented));
+
+            Console.WriteLine("Series = " + (string)response["comics"][0]["series"]);
+
+            if (!series.Equals("The Fuse", StringComparison.OrdinalIgnoreCase))
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetComicPageTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            Byte [] response = cs.GetComicPage("98");
+
+            if (response.Length > 0)
+            {
+                using (Image image = Image.FromStream(new MemoryStream(response)))
+                {
+                    image.Save("output.jpg", ImageFormat.Jpeg);  // Or Png
+                }
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void SetBookmarkTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            cs.SetBookmark("98", "10");
+
+            var response = cs.GetComicInfo("98");
+            string lastread_page = (string)response["comics"][0]["lastread_page"];
+
+            if (!lastread_page.Equals("10"))
+            {
+                Assert.Fail();
+            }
+
+            cs.SetBookmark("98");
+        }
+
+        [TestMethod()]
+        public void GetThumbnailTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            Byte[] response = cs.GetThumbnail("98");
+
+            if (response.Length > 0)
+            {
+                using (Image image = Image.FromStream(new MemoryStream(response)))
+                {
+                    image.Save("thumbnail.jpg", ImageFormat.Jpeg);  // Or Png
+                }
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetComicFileTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            Byte[] file = cs.GetComicFile("98");
+
+            var response = cs.GetComicInfo("98");
+
+            string filename = System.IO.Path.GetFileName((string)response["comics"][0]["path"]);
+
+            if (file.Length > 0 && filename.Length > 0)
+            {
+                File.WriteAllBytes(filename, file);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetComicListTest()
+        {
+            // Test getting all comics
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            JObject list = cs.GetComicList();
+            JArray comics = (JArray)list["comics"];
+
+            Console.WriteLine("Found " + comics.Count + " comics");
+
+            if (comics.Count > 100)
+            {
+                // Test getting all of one series
+                list = cs.GetComicList("The Fuse");
+                comics = (JArray)list["comics"];
+
+                Console.WriteLine("Found " + comics.Count + " issues of The Fuse");
+
+                if (comics.Count == 0 && comics.Count > 100)
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void GetFoldersTest()
+        {
+            ComicStreamerSharp cs = new ComicStreamerSharp(BaseUrl);
+            JObject list = cs.GetFolders();
+
+            if (list.Count == 0)
+            {
+                Assert.Fail();
+            }
+        }
+
+    }
+}
